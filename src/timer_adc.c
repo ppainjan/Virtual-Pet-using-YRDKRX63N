798234e98b8a0039d56943e320d87930583f3531
pratikpainjane <pratikpainjane@gmail.com>
2017-06-29 09:00:34 -0400

    first commit

---
+++ src/timer_adc.c
@@ -0,0 +1,157 @@
+/*******************************************************************************
+* File Name     : timer_adc.c 
+* Version       : 1
+* Device(s)     : RX63N
+* Tool-Chain    : Renesas RX Standard Toolchain 1.0.0
+* Description   : Defines the timed ADC sample used in this tutorial.
+*******************************************************************************/
+/*******************************************************************************
+* History : DD.MM.YYYY     Version     Description
+*         : 24.02.2012     1.00        First release              
+*******************************************************************************/
+
+/*******************************************************************************
+* Project Includes
+*******************************************************************************/
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Board and MCU specific definitions. */
+#include "platform.h"
+/* Declares LED flash functions used in this file. */
+#include "flash_led.h"
+/* Provides declarations of functions defined in this file */
+#include "timer_adc.h"
+/* Provides declarations of ADC functions used in this file. */ 
+#include "S12ADC.h" 
+
+#include "cmt_periodic_multi.h"
+
+/*******************************************************************************
+Macro Definitions
+*******************************************************************************/
+#define TIMER_COUNT_100MS   9375 /* 0.1 second with 48MHz pclk / 512 */ 
+#define CHANNEL_1   1
+
+/*******************************************************************************
+* Local Function Prototypes
+*******************************************************************************/
+/* AD conversion complete callback function prototype delcaration */
+static void cmt_callback_readADC(void);
+
+/*******************************************************************************
+* Global Variables
+*******************************************************************************/
+/* Timer period global variable - time between LED flashes */
+volatile uint16_t g_timer_count = TIMER_COUNT_100MS; /* Initial value. */
+int draw = 0;
+int happy =100, hungry = 0 ; 
+/*******************************************************************************
+* Function name: timer_adc
+* Description  : The function configures a ADC unit and a timer unit. After each
+*                timer period the callback function cmt_callback_readADC, is 
+*                executed.
+* Arguments    : none
+* Return value : none
+*******************************************************************************/
+void timer_adc(void)
+{
+    /* Prepare the ADC unit for continuous conversion of pot VR1 value. */
+    S12ADC_init(); 
+    /* Start continuous ADC conversion. */
+    S12ADC_start();
+    
+    /* Prepare a CMT channel as a periodic timer. */
+    /* Initialize a CMT one-shot timer. */ 
+    cmt_init();
+    /* Set up the callback function on cmt channel 0 */   
+    cmt_callback_set(CHANNEL_1, &cmt_callback_readADC);
+    /* Start 100mS count on cmt channel 0. */
+    cmt_start(CHANNEL_1, g_timer_count);
+            
+} /* End of function timer_adc(). */
+
+
+/*******************************************************************************
+* Function name: cmt_callback_readADC
+* Description  : This callback function is to be called from the CMT interrupt.
+*                The function reads the ADC conversion value of the potentiometer
+*                VR1 setting and the time period of the CMT counter is updated 
+*                with a new value proportional to that setting. Then the toggle_led
+*                function is called. The result is that the LED toggle rate will 
+*                vary based on the pot setting.
+* Arguments    : none
+* Return value : none
+*******************************************************************************/
+static void cmt_callback_readADC(void)
+{
+    uint16_t adc_value;
+	int i = rand()/1000 ;
+    int j = rand()/100 ;
+	draw = 1;
+	if(happy > 1 )
+	{
+		if((j % 3) == 0)
+		{
+			happy = happy - 3;
+		}
+		else if((j % 3) == 1)
+		{
+			happy = happy - 2;
+		}
+		else if((j % 3) == 2)
+		{
+			happy = happy - 1;
+		}
+		else 
+		{
+			happy = happy - 1;
+		}			
+	} 
+	else
+	{
+		happy = 0;	
+	} 
+	if(hungry < 100)
+	{
+		 if((i % 3) == 0)
+		{
+			hungry = hungry + 3;
+		}
+		else if((i % 3) == 1)
+		{
+			hungry = hungry + 2;
+		}
+		else if((j % 3) == 2)
+		{
+			hungry = hungry + 1;
+		}
+		else 
+		{
+			hungry = hungry + 1;
+		}
+	}
+	else
+	{
+		hungry = 100;	
+	}	
+	cmt_stop(CHANNEL_1); /* Stop the CMT counting. */ 
+        
+    /* Read the ADC conversion result. */
+    adc_value = S12ADC_read();
+
+    /* Recalculate the new timer period to make the flash period visible. */
+    /* Max ADC value is 4096 (12 bits). Timer count must not exceed 16 bit value. */  
+    g_timer_count = (adc_value * 10)+ 2000; /* Scale the delay and insure non-zero value. */
+
+    /* Start new count on cmt channel 0. */
+    cmt_start(CHANNEL_1, g_timer_count);    
+
+    /* Execute the toggle LEDs function */
+   // toggle_led();
+    
+} /* End of function cmt_callback_readADC(). */
+
+     
---
+++ src/tutorial_main.c
@@ -0,0 +1,394 @@
+
+/*******************************************************************************
+Includes   <System Includes> , "Project Includes"
+*******************************************************************************/
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include "platform.h" 
+//#include "r_switches.h"
+#include "cmt_periodic_multi.h"
+#include "timer_adc.h"
+//#include "flash_led.h"
+
+/*******************************************************************************
+Macro Definitions
+*******************************************************************************/
+#define TIMER_COUNT_600MS 56250  /* 600mS with 48MHz pclk/512 */
+#define MCU_NAME "   RX63N    "
+#define CHANNEL_0   0
+#define CHANNEL_1   1
+
+/*******************************************************************************
+External functions and public global variables. 
+*******************************************************************************/
+
+
+
+/*******************************************************************************
+Private function prototypes and global variables
+*******************************************************************************/
+void updatescreen(void);
+void delay(void);
+void delay1(void);
+extern int draw;
+extern int happy, hungry;
+int animate = 1;
+int eat = 5;
+char *str[50],*str1[50];
+/******************************************************************************
+* Function name: main
+* Description  : Main program function. The function first initialises the debug
+*                LCD, and displays the splash screen on the LCD. The function
+*                then calls the flash_led, timer_adc and statics_test functions. 
+*                These start by flashing the LEDs at a fixed rate until a switch is
+*                pressed, then the flash rate varies with the position of the   
+*                pot, RV1. Finally, statics_test displays " STATICS " on the    
+*                debug LCD, which is replaced with "TEST TEST" a letter at a 
+*                time.
+* Arguments    : none
+* Return value : none
+******************************************************************************/
+void main(void)
+{
+	int flag1 = 0,flag2 = 1, set = 0, set1= 0, set2= 0;
+	int i,j = 0, k = 0,l = 0;
+	int value[10] = {70,60,40,20,10,0};
+    /* Initialize the debug LCD on the RSPI bus. */
+    lcd_initialize();
+    
+    /* Clear LCD */
+    lcd_clear();
+    
+    /* Display message on LCD */
+    lcd_display(LCD_LINE1, "  RENESAS   ");
+    lcd_display(LCD_LINE2, MCU_NAME);
+
+    cmt_init(); /* Initialize the CMT unit for application timing tasks. */ 
+	timer_adc();
+	lcd_clear();	
+    while (1)
+    {
+		if(SW1 == 0)
+		{
+			delay();
+		    if(SW1 == 0)
+			{
+				flag1 = flag1 + 1;
+				set = set + 1; 
+			}
+		}
+		if(SW2 == 0)
+		{
+			delay();
+		    if(SW2 == 0)
+			{
+				set1 = set1 + 1; 
+				flag2 = flag2 - 1;
+				if(flag2 == 0)
+					flag2 = 3;
+			}
+		}
+		if(SW3 == 0)
+		{
+			delay();
+		    if(SW3 == 0)
+			{
+				set2 = set2 + 1; 
+				flag2 = flag2 + 1;
+				if(flag2 == 4)
+					flag2 = 0;
+			}
+		}	
+		delay();
+		if(flag1 % 2 == 1)
+		{	
+			switch(flag2)
+			{
+				case 1 :
+						delay();
+						delay();
+						if(set > 1)
+						{
+							j = 1;
+							//flag1 = 0;						
+						}	
+						delay();
+						if(j == 0)
+						{	
+							set2 = 0;
+							set1 = 0;
+							lcd_clear();
+							Set_Font_8_by_8();
+							lcd_display(LCD_LINE1, "MENU -  ");
+							lcd_display(LCD_LINE2,">.Feed Pet");
+		    				lcd_display(LCD_LINE3,"2.Play Pet ");
+							lcd_display(LCD_LINE4,"3. Exit");
+							sprintf(str,"Happy:%d",happy);
+							sprintf(str1,"Hungry:%d",hungry);
+							lcd_display(LCD_LINE6, str);
+							lcd_display(LCD_LINE7,str1);
+						}
+						else
+						{
+							k = k + 1;
+							if( hungry < 0 )
+							{
+								l = 1;
+								j = 0;
+								set = 0; 
+								lcd_clear();
+								hungry = 0;
+								happy = happy + 15;
+								flag1 = 0;
+							}
+							else
+							{	
+								delay();
+								delay();
+								lcd_clear();
+								Set_Font_Bitmap();
+								Set_LCD_Pos(20,5);
+								// Select Animation Frame
+								if(eat == 5)
+								eat = 6;
+								else
+								eat = 5;
+								Set_LCD_Char(eat);
+								//draw = 0;
+								hungry = hungry - 5;
+							}			
+						}
+						break;
+		
+				case 2 :
+						delay();
+						delay();
+						if(set > 1)
+						{
+							j = 1;
+							//flag1 = 0;						
+						}	
+						delay();
+						if(j == 0)
+						{
+						set2 = 0;
+						set1 = 0;		
+						lcd_clear();
+						Set_Font_8_by_8();
+						lcd_display(LCD_LINE1, "MENU -  ");
+						lcd_display(LCD_LINE2,"1.Feed Pet");
+		    			lcd_display(LCD_LINE3,">.Play Pet");
+						lcd_display(LCD_LINE4,"3. Exit");
+						sprintf(str,"Happy:%d",happy);
+						sprintf(str1,"Hungry%d",hungry);
+						lcd_display(LCD_LINE6, str);
+						lcd_display(LCD_LINE7,str1);
+						}
+						else
+						{
+							int random = rand()/100;
+							lcd_clear();
+							Set_Font_Bitmap();
+							if((random % 2) == 0)
+							{
+								Set_LCD_Pos(40,5);
+							}
+							else
+							{
+								Set_LCD_Pos(40,15);	
+							}
+							Set_LCD_Char(1);
+							delay();
+							delay();
+							delay();
+							delay();
+							delay();
+							delay();
+							delay();
+							delay();
+							if(SW2 == 0)
+							{
+								delay();
+								if(SW2 ==0)
+								{
+									lcd_clear();
+									Set_Font_Bitmap();
+									Set_LCD_Pos(5,5);
+									Set_LCD_Char(9);
+									delay1();
+									delay();
+									lcd_clear();
+									Set_Font_Bitmap();
+									Set_LCD_Pos(20,5);
+									Set_LCD_Char(9);
+									delay1();
+									delay();
+									lcd_clear();
+									Set_Font_Bitmap();
+									Set_LCD_Pos(40,5);
+									Set_LCD_Char(8);
+									delay1();
+									j = 0;
+									set = 0; 
+									set1 = 0; 
+									lcd_clear();
+									hungry = hungry + 15;;
+									happy = happy + 20;
+									if(happy > 100)
+									{
+										happy = happy - 5;
+									}
+									flag1 = 0;
+								}	
+							}
+							if(SW3 == 0)
+							{
+								delay();
+								if(SW3 == 0 )
+								{
+									lcd_clear();
+									Set_Font_Bitmap();
+									Set_LCD_Pos(5,20);
+									Set_LCD_Char(9);
+									delay1();
+									delay();
+									lcd_clear();
+									Set_Font_Bitmap();
+									Set_LCD_Pos(20,20);
+									Set_LCD_Char(9);
+									delay1();
+									delay();
+									lcd_clear();
+									Set_Font_Bitmap();
+									Set_LCD_Pos(40,20);
+									Set_LCD_Char(8);
+									delay1();
+									j = 0;
+									set = 0;
+									set2 = 0; 
+									lcd_clear();
+									hungry = hungry + 15;
+									happy = happy + 20; 
+									if(happy > 100)
+									{
+										happy = happy - 5;
+									}	
+									flag1 = 0;								
+								}
+							}	 			
+						}
+						break;
+								
+				case 3 :
+						set2 = 0;
+						set1 = 0;
+						lcd_clear();
+						Set_Font_8_by_8();
+						lcd_display(LCD_LINE1, "MENU -  ");
+						lcd_display(LCD_LINE2,"1.Feed Pet");
+		    			lcd_display(LCD_LINE3,"2.Play Pet ");
+						lcd_display(LCD_LINE4,">. Exit");
+						sprintf(str, "Happy:%d",happy);
+						sprintf(str1, "Hungry:%d",hungry);
+						lcd_display(LCD_LINE6, str);
+						lcd_display(LCD_LINE7,str1);
+						 if(set > 1)
+						{
+							set = 0;
+							flag1 = 0;
+						}
+						break;
+			}
+		}	
+		else
+		{
+			if(draw)
+	    	{
+				if(hungry == 100)
+				{
+					cmt_stop(CHANNEL_1);
+					lcd_clear();
+					Set_Font_Bitmap();
+					Set_LCD_Pos(20,5);
+					Set_LCD_Char(7);
+					Set_Font_8_by_8();
+					sprintf(str, "Happy:%d",happy);
+					sprintf(str1, "Hungry:%d",hungry);
+					lcd_display(LCD_LINE6, str);
+					lcd_display(LCD_LINE7,str1);	
+				}
+				else if(happy == 0)	
+				{
+					cmt_stop(CHANNEL_1);
+					for(i = 0 ; i < 6 ; i++)
+					{
+						lcd_clear();
+						Set_Font_Bitmap();
+						Set_LCD_Pos(value[i],5);
+						Set_LCD_Char(1);
+						Set_Font_8_by_8();
+						sprintf(str, "Happy:%d",happy);
+						sprintf(str1, "Hungry:%d",hungry);
+						lcd_display(LCD_LINE6, str);
+						lcd_display(LCD_LINE7,str1);
+						j = j + 1;
+						delay1();
+					}
+					if(j > 5)
+					{
+						lcd_clear();
+				    	Set_Font_8_by_8();
+						lcd_display(LCD_LINE6,"Bye Bye");
+						delay1();
+					}		
+				}
+				else
+				{
+					if((hungry < 100) && (happy > 0))
+					{
+						lcd_clear();
+						updatescreen();
+						Set_Font_8_by_8();
+						sprintf(str, "Happy:%d",happy);
+						sprintf(str1, "Hungry:%d",hungry);
+						lcd_display(LCD_LINE6, str);
+						lcd_display(LCD_LINE7,str1);
+					}	
+				}		
+			}
+		}					
+	}
+} /* End of function main(). */
+
+
+void updatescreen(void)
+{
+	lcd_clear();
+	Set_Font_Bitmap();
+	Set_LCD_Pos(20,5);
+	// Select Animation Frame
+	if(animate == 1)
+		animate = 2;
+	else
+		animate = 1;
+	Set_LCD_Char(animate);
+	draw = 0;
+}	
+
+void delay() //Delay loop
+{
+	int i,j;
+	for(i=0;i<300;i++)
+		for(j=0;j<5000;j++);	
+}	
+
+void delay1() // Long time Delay loop
+{
+	int i,j;
+	for(i=0;i<15000;i++)
+		for(j=0;j<5000;j++);	
+}	
+	
